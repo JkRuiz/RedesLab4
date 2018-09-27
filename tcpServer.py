5f758bb2351387190e3b2ece11509f852ea0d777
Jorge Gomez <jandresgomez@Jorges-MacBook-Pro-2.local>
2018-09-27 16:22:24 -0500

    Same log for all clients

--- tcpServer.py
+++ tcpServer.py
@@ -6,34 +6,40 @@ import json
 from threading import Thread
 
 
-def threaded_function(conn, addr, host):
-    with open(addr + "_" + host + logPrefix, 'w') as log:
-        log.write("Connection started at: " + datetime.datetime.now().time())
-        data = conn.recv(buffersize).decode('utf-8')
-        print('Server received', repr(data))
-        log.write("C:" +  data.decode())
-        
-        conn.send(str(time.time()).encode('utf-8'))
-        with open(fileName, 'rb') as afile:
-            buf = afile.read()
-            hasher.update(buf)
-        hasheado = hasher.hexdigest() + '\r\n'
-        conn.send(hasheado.encode('utf-8'))
-        f = open(fileName, 'rb')
-        l = f.read(buffersize)
-        while (l):
-            conn.send(l)
-            print('Sent ', repr(l))
-            l = f.read(buffersize)
-        f.close()
+def threaded_function(conn, addr, id):
+    sout("C" + str(id) + ": Connection started at: " + str(datetime.datetime.now()), id)
+    data = conn.recv(chunkSize).decode('utf-8')
+    sout("C" + str(id) + ": " + data, id)
 
-        print('Done sending')
-        conn.close()
+    rsp = "Sending " + fileName
+    sout("S: " + rsp)
+    conn.send()
+
+    conn.send(str(time.time()).encode('utf-8'))
+    with open(fileName, 'rb') as afile:
+        buf = afile.read()
+        hasher.update(buf)
+    hasheado = hasher.hexdigest() + '\r\n'
+    conn.send(hasheado.encode('utf-8'))
+    f = open(fileName, 'rb')
+    l = f.read(chunkSize)
+    while (l):
+        conn.send(l)
+        print('Sent ', repr(l))
+        l = f.read(chunkSize)
+    f.close()
+
+    print('Done sending')
+    conn.close()
 
 def getProperties():
     with open('configTCP.ini', 'r') as file:
         properties = json.load(file)
     return properties
+
+def sout(l):
+	log.write(l + '\n')
+	print(l)
 
 properties = getProperties()
 fileName = properties['fileName']
@@ -46,19 +52,18 @@ serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
 serverSocket.bind((host, port))  # Bind to the port
 serverSocket.listen(numberClients)  # Now wait for client connection.
-print ('Server listening....')
 
-threads = []
-j = 0
-hasher = hashlib.md5()
-print('out')
-for j in range(numberClients):
-    print('in')
-    conn, addr = serverSocket.accept()
-    print('Server adopted connection #' + j)
-    thread = Thread(target=threaded_function, args=(conn, addr, socket.gethostname()))
-    thread.start()
-    threads.append(thread)
+with  open((logPrefix), 'w') as log:
+	sout('Server listening....')
 
-for i in range(len(threads)):
-    threads[i].join()
+	threads = []
+	hasher = hashlib.md5()
+	for j in range(numberClients):
+	    conn, addr = serverSocket.accept()
+	    sout('Server adopted connection #' + str(j))
+	    thread = Thread(target=threaded_function, args=(conn, addr, j))
+	    thread.start()
+	    threads.append(thread)
+
+	for i in range(len(threads)):
+	    threads[i].join()
