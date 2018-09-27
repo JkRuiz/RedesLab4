0f14b11b67fe5caf38aac8db098d659d39dd975b
Jandresgomez16 <ja.gomez1@uniandes.edu.co>
2018-09-27 15:49:19 -0500

    Log for tcp server

--- tcpServer.py
+++ tcpServer.py
@@ -1,50 +1,62 @@
 import socket
 import time
 import hashlib
+import datetime
+import json
 from threading import Thread
 
 
-def threaded_function(conn):
-    data = conn.recv(buffersize).decode('utf-8')
-    print('Server received', repr(data))
-    conn.send(str(time.time()).encode('utf-8'))
-    with open(filename, 'rb') as afile:
-        buf = afile.read()
-        hasher.update(buf)
-    hasheado = hasher.hexdigest() + '\r\n'
-    conn.send(hasheado.encode('utf-8'))
-    f = open(filename, 'rb')
-    l = f.read(buffersize)
-    while (l):
-        conn.send(l)
-        print('Sent ', repr(l))
+def threaded_function(conn, addr, host):
+    with open(addr + "_" + host + logPrefix, 'w') as log:
+        log.write("Connection started at: " + datetime.datetime.now().time())
+        data = conn.recv(buffersize).decode('utf-8')
+        print('Server received', repr(data))
+        log.write("C:" +  data.decode())
+        
+        conn.send(str(time.time()).encode('utf-8'))
+        with open(fileName, 'rb') as afile:
+            buf = afile.read()
+            hasher.update(buf)
+        hasheado = hasher.hexdigest() + '\r\n'
+        conn.send(hasheado.encode('utf-8'))
+        f = open(fileName, 'rb')
         l = f.read(buffersize)
-    f.close()
-    print('Done sending')
-    conn.close()
+        while (l):
+            conn.send(l)
+            print('Sent ', repr(l))
+            l = f.read(buffersize)
+        f.close()
 
+        print('Done sending')
+        conn.close()
 
-filename = 'mytext.txt'
-numberClients = 1
-port = 60000
-buffersize = 1024
+def getProperties():
+    with open('configTCP.ini', 'r') as file:
+        properties = json.load(file)
+    return properties
+
+properties = getProperties()
+fileName = properties['fileName']
+numberClients = int(properties['numberClients'])
+port = int(properties['serverPort'])
+chunkSize = int(properties['chunkSize'])
+logPrefix = properties['logPrefix'] + str(numberClients) + ".txt"
+
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
 serverSocket.bind((host, port))  # Bind to the port
-serverSocket.listen(1)  # Now wait for client connection.
+serverSocket.listen(numberClients)  # Now wait for client connection.
 print ('Server listening....')
+
 threads = []
 j = 0
-conns = []
 hasher = hashlib.md5()
-
-while j < numberClients:
+print('out')
+for j in range(numberClients):
+    print('in')
     conn, addr = serverSocket.accept()
-    conns.append(conn)
-    j += 1
-
-for i in range(numberClients):
-    thread = Thread(target=threaded_function, args=(conns[i],))
+    print('Server adopted connection #' + j)
+    thread = Thread(target=threaded_function, args=(conn, addr, socket.gethostname()))
     thread.start()
     threads.append(thread)
 
