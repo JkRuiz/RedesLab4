d8e564040c21b35884f6641d171f7e4f382a0c8a
Jorge Gomez <jandresgomez@Jorges-MBP-2.localdomain>
2018-09-30 17:11:00 -0500

    Done with adding config files

---
+++ load.py
@@ -0,0 +1,17 @@
+import os
+import json
+
+def loadData():
+	with open('/config.txt', 'r') as f:
+		clients = json.load(f)
+	return clients
+
+def copy_to_client(addr):
+	os.system('sshpass -p "' + properties['clientPassword'] + '"" ssh -o StrictHostKeyChecking=no ' + properties['clientUsername'] + '@' + addr + ' "rm RedesLab4"')
+	os.system('sshpass -p "' + properties['clientPassword'] + '"" ssh -o StrictHostKeyChecking=no ' + properties['clientUsername'] + '@' + addr + ' "rm R_*"')
+	os.system('sshpass -p "' + properties['clientPassword'] + '"" ssh -o StrictHostKeyChecking=no ' + properties['clientUsername'] + '@' + addr + ' "git clone https://github.com/JkRuiz/RedesLab4"')
+
+clients = loadData()
+clientIPs = clients['clientIPs']
+for ip in clientIPs:
+	copy_to_client(ip)
