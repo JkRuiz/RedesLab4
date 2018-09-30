11a29d7842b9c8f7969291601330b25c80a2bc29
Jorge Gomez <jandresgomez@Jorges-MBP-2.localdomain>
2018-09-29 22:47:38 -0500

    Trying to fix tcpTestRun.py

--- tcpTestRun.py
+++ tcpTestRun.py
@@ -22,11 +22,42 @@ def setupServer(ssh):
 def shellSetup(ssh):
     chan = ssh.invoke_shell()
 
+    run_cmd(chan, ['su', properties['suPassword']])
+    run_cmd(chan, 'iptraf -s eth0')
+    run_cmd(chan, chr(127)*999)
+    run_cmd()
+
+    emptyChannel(chan)
+
+def run_cmd(chan, cmd):
+    stdin = chan.makefile('wb')
+
+    for x in cmd:
+        stdin.write(x + '\n')
+    stdin.flush()
+    #emptyChannel(chan)
+
+def emptyChannel(chan):
+    while not chan.recv_ready():
+        i = 0
+    while True:
+        try:
+            print(chan.recv(4096).decode())
+        except:
+            print('broke down')
+            break
+
+def waitChannel(chan):
+    while not chan.recv_ready():
+        i = 0
+
+
 properties = getProperties()
 serverSSH = paramiko.SSHClient()
 serverSSH.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+print('Logging in..')
 serverSSH.connect(properties['serverIp'], username=properties['serverUsername'], password=properties['serverPassword'])
 print('Login - OK')
-setupServer(serverSSH)
+shellSetup(serverSSH)
 serverSSH.close()
 print('DONE')
