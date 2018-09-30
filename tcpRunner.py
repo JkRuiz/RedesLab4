1427f06831e42717718b7059a580ddf627708415
Jorge Gomez <jandresgomez@Jorges-MBP-2.localdomain>
2018-09-30 00:49:54 -0500

    Trying with invoked shell

--- tcpRunner.py
+++ tcpRunner.py
@@ -6,13 +6,20 @@ from threading import Thread
 def run_server():
 	import tcpServer
 
+def run_cmd(chan, cmd):
+    stdin = chan.makefile('wb')
+    stdin.write(cmd + '\n')
+    stdin.flush()
+
 def run_client(ip):
 	ssh = paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	ssh.connect(ip, username=properties['clientUsername'], password=properties['clientPassword'])
 	print('logged to client == ', str(ip))
 	
-	stdin, stdout, stderr = ssh.exec_command('python3 RedesLab4/tcpClient.py')
+	chan = ssh.invoke_shell()
+	run_cmd(chan, 'cd RedesLab4')
+	run_cmd(chan, 'python3 tcpRunner.py')
 	
 	ssh.close()
 
