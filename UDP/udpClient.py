52d138c2aaf86fcfaa7a4873b4bffe3b18008dc0
Maria del Rosario León <mdr.leon10@uniandes.edu.co>
2018-09-30 20:57:16 -0500

    UDP con threads

--- UDP/udpClient.py
+++ UDP/udpClient.py
@@ -1,27 +1,41 @@
 import socket
 
+#genera un socket UDP
 cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#direccion del servidor
 serverAdr = ('localhost', 9000)
-
+#envia mensaje al servidor
 cliente.sendto('status OK'.encode(), serverAdr)
 
+#booleano que determina se ya se termino de recivir el archivo
 hay = True
+#guarda el mensaje a medida que va llegando
 mensajeTotal = ""
+#contador de paquetes que recibe
 i = 0
+#Abre el archivo que va a guardar la información recibida
+f = open('recepción.txt', 'a')
+#while para recibir y enviar mensajes
 while hay:
-
+	#envia mensaje al servidor
 	message, addrSerer = cliente.recvfrom(1024)
-
+	#recibe el mensaje y lo guarda en la
 	if "acabe" not in message.decode():
-
+		#se incrementa el numero de paquetes recibidos
 		i = i +1
+		#se añade el mensaje que llego al que ya había
 		mensajeTotal = mensajeTotal + message.decode()
-
+		print(mensajeTotal)
+	#Si el mensaje contiene "acabe" se cambia ha false la variable booleana y se incrementa el numero de paquetes
 	else:
-
+		#cambio de la variable
 		hay = False
+		#Incrementa el número de paquetes
 		i = i + 1 
-
-print(mensajeTotal)
-print (i)
+#Escribe el archivo.
+f.write(mensajeTotal)
+#Cierra el archivo.
+f.close()
+#print(mensajeTotal)
+#print (i)
 cliente.sendto(str(i).encode(), serverAdr)
--- UDP/udpServer.py
+++ UDP/udpServer.py
@@ -1,75 +1,93 @@
 import socket
 import json
 import time
+from threading import Thread
 
 #Obtiene las propiedades del servidor del archivo configUDP.txt
 def getProperties():
     with open('configUDP.txt', 'r') as file:
         properties = json.load(file)
     return properties
+def threaded_function(id):
+	print(id)
+	#contador de paquetes
+	i = 0
+	#booleano para saber hasta cuando debe seguir activo el while.
+	hay = True
+	#recepción y envio de mensajes.
+	while hay:
+		#recibe los datos del socket y la dirección del cliente.
+		data, addr = serverSocket.recvfrom(1024)
+		print (data.decode())
+		#abre el archivo que el cliente envio.
+		f = open(fileName)
 
-#Obtiene las propiesdades
+		outputData = " "
+
+		#while para el envio del archivo.
+		while (outputData):
+			#chunk de información que se enviara. 
+			outputData = f.read(chunkSize)
+			#envio de l segmento del archivo.
+			serverSocket.sendto(outputData.encode(), addr)
+			#imprime la información enviada.
+			#print(outputData)
+			#aumenta el contador de paquetes enviados.
+			i = i + 1
+			#imprime el contador.
+			#print (i)
+		#cierra el archivo.
+		f.close()
+		#envia mensaje para que el cliente sepa que se termino la transmicion.
+		outputData = "acabe"
+		#imprime el mensaje de terminar.
+		#print(outputData)	
+		#aumenta el contador de paquetes.
+		i = i+1
+		#envia el mensaje de termino.
+		serverSocket.sendto(outputData.encode(), addr)
+		#imprime el contador de mensajes.
+		#print (i)
+		# recive el numero de pauqetes recibidos.
+		data, addr = serverSocket.recvfrom(1024)
+		#imprime el numero de paquetes enviados.
+		#print(data.decode())
+		#indica la terminación del while.
+		hay = False
+
+#Obtiene las propiesdades.
 properties = getProperties()
-#Nombre del archivo que se va a enviar
+#Nombre del archivo que se va a enviar.
 fileName = properties['fileName']
-#Numero de clientes a los que se escuchara
+#Numero de clientes a los que se escuchara.
 numeroClientes = int(properties['numberClients'])
-#Numero de puerto que se utilizara
+#Numero de puerto que se utilizara.
 port= int (properties['serverPort'])
-#Tamaño de los paquetes que se van a usar
+#Tamaño de los paquetes que se van a usar.
 chunkSize = int(properties['chunkSize'])
-#Genera el log de la transacción
+#Genera el log de la transacción.
 logPrefix = properties['logPrefix'] + str(numeroClientes) + "_" + str(time.time()) + ".log"
 
 
-#genera un socket UDP
+#genera un socket UDP.
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#Obtiene el hostname sobre el que se ejecuta el programa
+#Obtiene el hostname sobre el que se ejecuta el programa.
 host = socket.gethostname()
-#la dirección y el puerto por donde el servidor va a escuchar
+#la dirección y el puerto por donde el servidor va a escuchar.
 serverSocket.bind(('', port))
 
-#contador de paquetes
-i = 0
-#booleano para saber hasta cuando debe seguir activo el while.
-hay = True
-#recepción y envio de mensajes.
-while hay:
-	#recibe los datos del socket y la dirección del cliente
-	data, addr = serverSocket.recvfrom(1024)
-	print (data.decode())
-	#abre el archivo que el cliente envio
-	f = open(fileName)
+#arreglo de threads
+threads = []
+#for que inicializa los threads.
+for j in range (numeroClientes):
+	thread = Thread(target=threaded_function, args=(j,))
+	thread.start()
+	threads.append(thread)
+#for que sincroniza los threads.
+for i in range(len(threads)):
+	threads[i].join() 
 
-	#while para el envio del archivo
-	while (outputData):
 
-		#chunk de información que se enviara 
-		outputData = f.read(chunkSize)
-		#envio de l segmento del archivo
-		serverSocket.sendto(outputData.encode(), addr)
-		#imprime la información enviada
-		print(outputData)
-		#aumenta el contador de paquetes enviados
-		i = i + 1
-		#imprime el contador
-		print (i)
-	#envia mensaje para que el cliente sepa que se termino la transmiciom.
-	outputData = "acabe"
-	#imprime el mensaje de terminar.
-	print(outputData)	
-	#aumenta el contador de paquetes
-	i = i+1
-	#envia el mensaje de termino
-	serverSocket.sendto(outputData.encode(), addr)
-	#imprime el contador de mensajes
-	print (i)
-	# recive el numero de pauqetes recibidos
-	data, addr = serverSocket.recvfrom(1024)
-	#imprime el numero de paquetes enviados
-	print(data.decode())
-	#indica la terminación del while
-	hay = False
 
 
 
